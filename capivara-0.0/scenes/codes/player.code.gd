e3bf14b74e3dc9b133ff872774b4098a416c6b86
neanthertal-albino <iagogeneral2@gmail.com>
2026-05-21 09:20:15 -0300

    muitass atualizações

--- capivara-0.0/scenes/codes/player.code.gd
+++ capivara-0.0/scenes/codes/player.code.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 #variaveis
 var grav = 15  #grav: valor da gravidade do player
 var speed = 350  #speed: valor da velocidade do player (o tanto q ele anda)
-var tim_o_meter = 0.0  #tim_o_meter: ele é só um contador (anteriormente eu pensei nele como um controle pro player poder escolher a altura em q pulasse, só q dai ele aumentou o pulo de um outra forma (o segundo pulo fica maior se ficar segurando K), gostei e deixei no jogo mesmo :D)
 var tim_coyote = 0.0  #tim_coyote: ele é basicamente o mesmo q o "tim_o_meter", só q ele funciona para quando ele estiver caindo ter um atraso antes de cair (igual o coyote do desenho do papaléguas, bep bep!) e tambem da pra controlar a altura do pulo.
 var is_falling = false  #is_falling: isso apenas mostra pro código se o player pulou ou nao
 var jumped = false  #jumped: mesma coisa do "falling", só q ele mostra se o player pulou.
@@ -13,6 +12,8 @@ var shake_strength = 0.0  #forca do shake (shake = tremer pros bot dos ingreis h
 var contador_shake = 0.0  #conta quando o shake pode ou nao acontecer
 var permetidor_shake = false  #permite ou nao o contador shake contar
 var rezet_shake = false   #rezeta o contador shake
+var c_shake = 0 # conta o quantas vezes o botão pra efetuar o ass_power foi apertado
+var c_shake_cool = 0.0 # conta os milissegundos pra considerar double click
 @onready var camera = $Camera2D  #é a camera
 var pode_jumpwall = false  #ele permite se pode jumpwall ou nao
 var contador_jumpwall = 0.0  #ele conta por quanto tempo o jumpwall ficara verdadeiro.
@@ -22,8 +23,6 @@ var stage_run = 0 # estagios de velocidade da corrida (de 0 a 2)
 var conta_stage_run:int = 0 # tempo de cooldown entre os estagios
 var plan = false # verifica se o player planou
 var cont_vel_pstg = 0
-var direcao = 0
-var stop_run = 0.0
 
 #o "func _physics_process(_delta):" roda tudo oque tiver nele 60 vezes por segundo (muita coisa né?) 
 func _physics_process(_delta: float) -> void:
@@ -48,6 +47,8 @@ func _physics_process(_delta: float) -> void:
 	if global_position.y > 1000:  # ou qualquer valor fundo do seu mapa
 		global_position = Vector2(160, 464)  # volta pro comeco
 		velocity = Vector2.ZERO  # zera velocidade pra evitar bug de queda
+	
+	print(c_shake)
 	
 	#aqui estao as funcoes que criei.
 	_plane()
@@ -92,7 +93,6 @@ func _plane():
 func _move_basics():
 	#se apertar D e o "coldown_ass_power" for 0, o eixo x sera igual o valor da variavel speed (o palyer anda).
 	if Input.is_key_pressed(KEY_D):
-		direcao = 0
 		if !Input.is_key_pressed(KEY_S):
 			if couldown_ass_power == 0:
 				#isso só deixa o sprite do player pra direita.
@@ -114,7 +114,6 @@ func _move_basics():
 			velocity.x = move_toward(velocity.x, 0, 40)
 	#isso é a mesa coisa, so q com o A (q dai é pra esquerda)
 	elif Input.is_key_pressed(KEY_A):
-		direcao = 1
 		if !Input.is_key_pressed(KEY_S):
 			if couldown_ass_power == 0:
 				$AnimatedSprite2D.flip_h = true
@@ -158,30 +157,41 @@ func _slide_run_wall():
 	
 #essa funcao aqui faz com q o player de um "ground pound" no chao, mas eu prefiro chamar de ass power mesmo.
 func _ASS_POWER():
-	#se apertar S e NAO estiver no chao e NAO estiver apertando K o eixo x do player aumenta continuamente em 400.
+	#isso aq conta quantas vezes a ação "ASS_POWER" foi apertada
+	if Input.is_action_just_pressed("ASS_POWER"):
+		c_shake += 1
+		c_shake_cool += 0.1
+	if c_shake_cool < 0.5:
+		c_shake = 0
+	#se apertar S e NAO estiver no chao e NAO estiver apertando K o eixo y do player aumenta continuamente em 400.
 	#(que faz ele cair bem rapido)
-	if Input.is_key_label_pressed(KEY_S) and !is_on_floor() and !Input.is_key_label_pressed(KEY_K):
-		if !is_on_wall():
-			velocity.y += 400
-			#e dai o "ass_powered" fica igual a true ("verdadeiro" pros nao bilingue, haha) e o couldown fica igual a 3.0
-			ass_powered = true
-			couldown_ass_power = 2.0
-			permetidor_shake = true
-	#e se tiver chego no chao o powered fica false ("falso" pros !bilingue, ha) e o contador comeca a diminuir 0.2
-	elif is_on_floor():
-		ass_powered = false
-		couldown_ass_power -= 0.1
-		shake_strength = 20
-		if permetidor_shake == true:
-			contador_shake += 0.1
-		
-		#dai se chegar a 0 ou menos (o ou menos eu coloquei so pra garantir q ele pare de diminuir mesmo) fica = 0
-		if couldown_ass_power <= 0:
-			couldown_ass_power = 0
-	#se nenhum desses dois acontecer e apertar S mesmo assim o couldown fica = 0
-	else:
-		if !Input.is_key_label_pressed(KEY_S):
-			couldown_ass_power = 0
+	if c_shake >= 2:
+		if Input.is_action_pressed("ASS_POWER") and !is_on_floor() and !Input.is_key_label_pressed(KEY_K):
+			if !is_on_wall():
+				velocity.y += 400
+				#e dai o "ass_powered" fica igual a true ("verdadeiro" pros nao bilingue, haha) e o couldown fica igual a 3.0
+				ass_powered = true
+				couldown_ass_power = 2.0
+				permetidor_shake = true
+		#e se tiver chego no chao o powered fica false ("falso" pros !bilingue, ha) e o contador comeca a diminuir 0.2
+		elif Input.is_action_pressed("ASS_POWER") and is_on_floor():
+			c_shake = 0
+			c_shake_cool = 0.0
+			ass_powered = false
+			couldown_ass_power -= 0.1
+			shake_strength = 20
+			if permetidor_shake == true:
+				contador_shake += 0.1
+			
+			#dai se chegar a 0 ou menos (o ou menos eu coloquei so pra garantir q ele pare de diminuir mesmo) fica = 0
+			if couldown_ass_power <= 0:
+				couldown_ass_power = 0
+		#se nenhum desses dois acontecer e apertar S mesmo assim o couldown fica = 0
+		else:
+			if !Input.is_key_label_pressed(KEY_S):
+				couldown_ass_power = 0
+				c_shake = 0
+				c_shake_cool = 0.0
 
 #essa funcao aqui faz o player pular, da pra ver q nao é tao simples por ter "apenas" 24 linhas de codigo, nao é? (sim, eu contei a linhas. [na verdade só diminui 30 por 52 mesmo e {diminui 1 (no final eu so copiei e colei no inicio mesmo)}, mas isso nao interessa, volta pro código!!! D:< ]).
 func _jump():
@@ -192,12 +202,6 @@ func _jump():
 			if Input.is_key_label_pressed(KEY_K) and !Input.is_key_label_pressed(KEY_W) and !Input.is_key_label_pressed(KEY_S):
 				jumped = true
 				velocity.y -= 300
-				#e dae se o tim_o_meter (tempometro ou temporizador) for maior ou igual a 1 ele diminue 100... (aparentemente se eu tirar isso aqui ferra com o codigo, entao vou deixa-lo aqui)
-				if tim_o_meter >= 1:
-					velocity.y -= 100
-					#se apertar K e o tim for igual ou maior a 2,vai diminuir 20 (fazendo o pulo ficar maior)
-					if Input.is_key_pressed(KEY_K) and tim_o_meter >= 2:
-						velocity.y -= 20
 	#se nao estiver em contato com o chao...
 	else:
 		#...e se o "jumped" for false e o tim_coyote for menor ou igual a 1, o eixo y diminue em 100 
@@ -269,10 +273,10 @@ func jumpwall():
 				contador_jumpwall += 0.1
 			#se aperta A durante tudo isso o player dara uma investida pra esquerda.
 			if Input.is_key_pressed(KEY_A):
-				velocity.x += -400
+				velocity.x += -40
 			#se aperta D durante tudo isso o player dara uma investida pra direita.
 			if Input.is_key_pressed(KEY_D):
-				velocity.x += 400
+				velocity.x += 40
 		elif Input.is_key_pressed(KEY_K) and is_on_wall():
 			contador_jumpwall = -0.5
 	#se estiver o chao o pode jumpwall fica falso
@@ -292,14 +296,14 @@ func _stage_run():
 	if cont_vel_pstg == 0:
 		if Input.is_key_label_pressed(KEY_J) and plan == false:
 			conta_stage_run +=  1
-			velocity0()
+
 			if conta_stage_run >= 150 and conta_stage_run < 350:
 				stage_run = 1
-				velocity0()
+
 			elif conta_stage_run >= 600:
 				stage_run = 2
 				conta_stage_run = 600
-				velocity0()
+
 		else:
 			conta_stage_run = 0
 			stage_run = 0
@@ -318,14 +322,3 @@ func cont_vel_pstgf():
 			cont_vel_pstg = 0
 	else:
 		cont_vel_pstg = 0
-
-func velocity0():
-	if velocity.x == 0:
-		conta_stage_run = 0
-		stage_run = 0
-
-
-
-	# direcao 0 = player virado pra direita; direcao 1 = player virado pra esquerda
-	
-	
